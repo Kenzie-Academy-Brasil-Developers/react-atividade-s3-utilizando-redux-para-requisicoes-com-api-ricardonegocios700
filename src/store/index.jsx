2730b47fe5eec1a94a92e707041c4b00e77c847f
Ricardo <ricardonegocios700@gmail.com>
2021-09-30 12:20:39 -0300

    criei a estrutura

---
+++ src/store/index.jsx
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+
+//importando o reducer criado
+//import fruitsReducer from "./modules/fruits/reducer";
+
+//combinando os reducers da aplicação
+//const reducers = combineReducers({ fruits: fruitsReducer });
+
+//passando os reducers combinados para a store
+//const store = createStore(reducers);
+
+export default store;
